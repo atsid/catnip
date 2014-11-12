b0f9b83e8ff9e456c6c5c8b7b2296fa06a5a4c4c
Jason Hinebaugh <jason@bootjackdesign.com>
2014-11-12 08:07:29 -0500

    Pulling the food prefs with correct sort and filter

--- Catnip/scripts/food.js
+++ Catnip/scripts/food.js
@@ -1,8 +1,36 @@
 $(function() {
+    var me, today;
+    
+    today = new Date().trimToDate();
+    me = '1bac2d70-69a0-11e4-8481-bd60c9634975';
+
 	config.set("food", pi.observable());
 	config.set("food.preferences", pi.data.DataSource.create({
-		source: "Everlive.DailyFoodPreferences"
-	})).fetch();
+		source: "Everlive.DailyFoodPreferences",
+        serverFiltering: true,
+        serverPaging: true,
+        page: 1,
+        pageSize: 3,
+        filter: [
+            {field: 'User', operator: 'eq', value: me},
+            {field: 'Date', operator: 'eq', value: today.toISOString()}
+        ],
+        sort: {field: 'Priority', dir: 'ascending'},
+        change: function () {
+            var data, saved;
+            data = this.data();
+            saved = {
+                first: data[0],
+                second: data[1],
+                third: data[2]
+            };
+            config.set('food.saved', saved);
+            $('input.genre').each(function () {
+                updateGenres.call(this);
+            });
+
+        }
+    })).fetch();
 	
 	var dataUriPrefix = 'http://api.everlive.com/v1/kD5Tly50Vf6nm8kn/';
 	var priorityToCategoryId = {};
@@ -21,15 +49,16 @@ $(function() {
 		genresBody.append(row);
 	}
 	
-	$('.genre').each(function() {
+    
+    function updateGenres(saved) {
 		var combobox = $(this);
 		var priority = combobox.attr('priority');
+        var saved = config.get('food.saved');
 		combobox.kendoComboBox({
 			dataTextField: "Name",
 			dataValueField: "Id",
 			dataSource: pi.data.DataSource.create({ // need one data source per combobox
 				source: "Everlive.FoodCategories",
-				
 			}),
 			filter: "contains",
 			suggest: true,
@@ -37,17 +66,17 @@ $(function() {
 				priorityToCategoryId[priority] = this.value();
 			}
 		});
-	});
-	
+	}
+    	
 	$('.set-genres').on('click', function() {
 		var uri = dataUriPrefix + 'DailyFoodPreferences';
 
-		var date = new Date();
+		var date = new Date().trimToDate();
 		
 		function getExisting() {
-			var dateStr = [date.getYear(), (date.getMonth() + 1), date.getDate()].join('-');
 			var filter = JSON.stringify({
-				Date: dateStr
+                User: me,
+				Date: date.toISOString()
 			});
 			$.get(uri + '?filter=' + filter)
 				// .done(deleteExisting); // TODO put back
@@ -68,12 +97,11 @@ $(function() {
 			}
 		}
 		function postNew() {
-			var dateStr = date.toISOString();
 			var newPrefs = Object.keys(priorityToCategoryId).sort().map(function(priority, normalizedPriority) {
 				return {
-					User: userId,
-					Date: dateStr,
-					PreferenceDate: dateStr,
+					User: me,
+					Date: date.trimToDate(),
+					PreferenceDate: date.toISOString(),
 					Priority: Number(normalizedPriority), // zero-based // TODO add Priority on persistence model
 					FoodCategory: priorityToCategoryId[priority]
 				};
