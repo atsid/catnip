43e26034cf151b65da1a77446b67175b61f7f166
Pete  Oliver-Krueger <pete@millionmunkeys.net>
2014-11-25 16:48:31 +0000

    Cleanup duplicate code.  Changes made by Pete  Oliver-Krueger (pete@millionmunkeys.net).

--- Catnip/scripts/preferences.js
+++ Catnip/scripts/preferences.js
@@ -1,86 +1,4 @@
 $(function() {
-    
-	/*
-	var $startTime, $endTime, data, startDate, endDate, today;
-    
-    // Modify the native Date object (very dangerous, I know, I know...)
-    Date.prototype.padNumber = function (num) {
-        if (num < 10) {
-            num = '0' + num;
-        }
-        return num.toString();
-    };
-    
-    Date.prototype.trimToDate = function () {
-        this.setHours('0');
-        this.setMinutes('0');
-        this.setSeconds('0');
-        this.setMilliseconds('0');
-        return this;
-    }
-    
-    Date.prototype.getShortDate = function () {
-        var day, month, year;
-        
-        day = this.padNumber(this.getDate());
-        month = this.padNumber(this.getMonth() + 1);
-        year = this.getFullYear();
-        
-        return [year, month, day].join('-');
-    };
-
-    Date.prototype.getShortTime = function (full) {
-        var arr, hours, minutes, str;
-        
-        hours = this.padNumber(this.getHours());
-        minutes = this.padNumber(this.getMinutes());
-        
-        arr = [hours, minutes];
-        if (full) {
-            arr.push('00');
-            str = arr.join(':');
-        } else {
-            str = arr.join('');
-        }
-        
-        return str;
-    };
-    
-    Date.prototype.toISOString = function () {
-        return this.getShortDate() + 'T' + this.getShortTime(true) + 'Z'
-    };
-    
-    // Some utility functions
-    function startTimeChangeHandler(e) {
-        var startDate = new Date($startTime.value());
-        if (startDate) {
-            startDate.setMinutes(startDate.getMinutes() + 30);
-            $endTime.min(startDate);
-            startDate.setMinutes(startDate.getMinutes() + 30);
-            $endTime.value(startDate);
-        }
-    }
-    
-    function endTimeChangeHandler(e) {
-        var startTime, endTime, restEndpoint, record;
-        
-        restEndpoint = 'http://api.everlive.com/v1/kD5Tly50Vf6nm8kn/DailyPreferences';
-        
-        today = new Date();
-        startTime = new Date($startTime.value());
-        endTime = new Date($endTime.value());
-        
-        record = preferences.options.get("selected");
-
-        record.set('StartTime', startTime.getShortTime());
-        record.set('EndTime', endTime.getShortTime());
-        console.log(record);
-                                
-        $.ajax(restEndpoint + '/' + record.Id, {method: 'put', data: record.toJSON()});
-        window.preferences.fetch();
-    }
-	*/
-    
     // Initialize variables
     var today = new Date();
 	today = [today.getFullYear(), ("0"+(today.getMonth()+1)).substr(-2), ("0"+today.getDate()).substr(-2)];
@@ -110,16 +28,6 @@ $(function() {
 		window.myPreferences.set("Id", "");
 		window.myPreferences.set("id", "");
 	}
-	/*
-	window.preferences.bind("requestEnd", function(e) {
-		if (e.response.Result) {
-			e.response.Result.forEach(function(item,index) {
-				item.StartTime = new Date(item.StartTime);
-				item.EndTime = new Date(item.EndTime);
-			});
-		}
-	});
-	*/
 	window.preferences.bind("change", function(e) {
 		if (e.action === "itemchange") {
 			if (e.field === "StartTime") {
