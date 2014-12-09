d5d722e8a1b86dec31c1adc9e56e907784b0c33e
Pete Oliver-Krueger <pete@millionmunkeys.net>
2014-12-09 23:06:26 +0000

    Bug fixes resulting from millisecond differences between local time and server time!  Changes made by Pete Oliver-Krueger (pete@millionmunkeys.net).

--- scripts/preferences.js
+++ scripts/preferences.js
@@ -129,14 +129,32 @@ $(function() {
 			window.preferences.timeout = window.setTimeout(window.preferences.cycle, 5 * 60 * 1000); // check every 5 minutes
 		}
 		
+		// Fix invalid data
+		window.preferences.options.bind("change", function(e) {
+			if (e.field === "selected") {
+				e.value = this.get(e.field);
+				if (e.value instanceof kendo.data.ObservableObject) {
+					// CAUTION: Convert User to Object
+					if (e.value.User === window.myAccount.get("Id"))
+						e.value.User = window.myAccount;
+					// CAUTION: Convert to javascript Date objects.
+					if (typeof(e.value.StartTime) === "string")
+						e.value.StartTime = new Date(e.value.StartTime);
+					if (typeof(e.value.EndTime) === "string")
+						e.value.EndTime = new Date(e.value.EndTime);
+					/*
+					if (typeof(e.value.ModifiedAt) === "string")
+						e.value.ModifiedAt = new Date(e.value.ModifiedAt);
+					if (typeof(e.value.CreatedAt) === "string")
+						e.value.CreatedAt = new Date(e.value.CreatedAt);
+					*/
+				}
+			}
+		});
+		
 		window.preferences.bind("requestEnd", function(e) {
 			if (window.myAccount && window.myAccount.Id && e.type === "read")
 				$('#lastsync').html("<label>Last Sync: <span class=\"modified\">"+new Date()+"</span></label>");
-		});
-		
-		window.preferences.options.bind("change", function(e) {
-			if (e.field === "selected" && this.get("selected") instanceof kendo.data.ObservableObject)
-				this.set("disabled", !!this.get("selected").get("OptOut"));
 		});
 		
 		window.preferences.bind("requestEnd", function(e) {
@@ -157,9 +175,8 @@ $(function() {
 							break;
 						}
 					}
-					var myModifiedAt = (window.myPreferences.ModifiedAt && window.myPreferences.ModifiedAt.getTime) ? window.myPreferences.ModifiedAt.getTime() : 0;
+					var myModifiedAt = (window.myPreferences && window.myPreferences.ModifiedAt && window.myPreferences.ModifiedAt.getTime) ? window.myPreferences.ModifiedAt.getTime() : 0;
 					if (!serverPreferences || myModifiedAt > serverPreferences.ModifiedAt.getTime()) {
-						this.options.set("selected", this.add(window.myPreferences));
 						window.myPreferences.dirty = true; // Make sure it syncs
 						if (serverPreferences) {
 							this.remove(serverPreferences);
@@ -167,10 +184,15 @@ $(function() {
 							if (serverPreferences.id === window.myPreferences.id)
 								this._destroyed.pop();
 						}
+						// WARNING: This must come AFTER removing serverPreferences.
+						// There is a bug where the timestamp on the server is less by milliseconds until the next GET.
+						// If we don't do this after, the remove() will delete both the server record and the local record!
+						this.options.set("selected", this.add(window.myPreferences));
 						this.sync();
 					} else {
 						window.myPreferences = this.options.set("selected", serverPreferences);
 					}
+					this.options.set("disabled", !!window.myPreferences.get("OptOut"));
 				}
 			} catch(e) {
 				e.event = "Find myPreferences";
@@ -178,12 +200,10 @@ $(function() {
 			}
 		}
 		
+		/*
+		*/
 		window.preferences.bind("change", function(e) {
 			try {
-				/*
-				if (e.action === "remove")
-					delete window.myPreferences;
-				*/
 				if (e.action === "sync" && e.items)
 					e.items.forEach(function(item,index) {
 						if (item.get("User") === window.myAccount.get("Id"))
@@ -201,20 +221,38 @@ $(function() {
 				this.view().forEach(function(daily,index) {
 					if (!daily.User || typeof(daily.User) === "string")
 						return;
-					if (daily.OptOut)
-						return;
-					if (daily.Brought) {
+					if (daily.OptOut) {
+						// undecided = false; // CAUTION: OptOuts shouldn't affect undecided.
+						results.push({
+							User: daily.User.Id,
+							DisplayName: daily.User.DisplayName,
+							Preference: 0,
+							Food: "Out",
+							sort: -1
+						});
+					}
+					else if (daily.Brought) {
 						// undecided = false; // CAUTION: Brought shouldn't affect undecided.
-						if (!food.Brought)
-							food.Brought = {};
-						food.Brought[daily.User.Id] = 1;
+						results.push({
+							User: daily.User.Id,
+							DisplayName: daily.User.DisplayName,
+							StartTimeCode: daily.StartTimeCode,
+							EndTimeCode: daily.EndTimeCode,
+							Preference: 1,
+							Food: "Brought",
+							sort: 0
+						});
 					} else if (daily.FoodCategories) {
 						daily.FoodCategories.forEach(function(id,index) {
 							undecided = false;
 							var record = window.food.get(id);
 							if (record) {
-								food[record.Name] = food[record.Name] || {};
-								food[record.Name][daily.User.Id] = 1;
+								food[record.Name] = food[record.Name] || { Users: {}, StartTimeCode: daily.StartTimeCode, EndTimeCode: daily.EndTimeCode };
+								food[record.Name].Users[daily.User.Id] = 1;
+								if (daily.StartTimeCode < food[record.Name].StartTimeCode)
+									food[record.Name].StartTimeCode = daily.StartTimeCode;
+								if (daily.EndTimeCode > food[record.Name].EndTimeCode)
+									food[record.Name].EndTimeCode = daily.EndTimeCode;
 							} else {
 								window.food.read(); // Maybe we're out of date
 							}
@@ -222,7 +260,7 @@ $(function() {
 					}
 				});
 				if (undecided)
-					food["Undecided"] = {};
+					food["Undecided"] = { Users: {}, StartTimeCode: "0000", EndTimeCode: "2400" };
 				// Build results
 				for (var pref in food) {
 					this.view().forEach(function(daily,index) {
@@ -230,17 +268,17 @@ $(function() {
 							return;
 						if (daily.OptOut)
 							return;
-						if ((pref !== "Brought" && daily.Brought) || (pref === "Brought" && !daily.Brought))
+						if (daily.Brought)
 							return;
-						var record = {
-							User: daily.User.Id,
-							DisplayName: daily.User.DisplayName,
-							Preference: food[pref][daily.User.Id], // CAUTION: Could be null
-							Food: pref
-						};
-						for (var timecode=parseInt(daily.StartTimeCode); timecode<daily.EndTimeCode; timecode=(timecode%100) ? timecode+=70 : timecode+=30)
-							record["utc"+timecode] = record.Preference ? "preference" : "available";
-						results.push(record);
+						if (daily.StartTimeCode < food[pref].EndTimeCode && daily.EndTimeCode > food[pref].StartTimeCode)
+							results.push({
+								User: daily.User.Id,
+								DisplayName: daily.User.DisplayName,
+								StartTimeCode: daily.StartTimeCode,
+								EndTimeCode: daily.EndTimeCode,
+								Preference: food[pref].Users[daily.User.Id], // CAUTION: Could be null
+								Food: pref
+							});
 					});
 				}
 				window.results.data(results);
@@ -250,71 +288,74 @@ $(function() {
 			}
 		});
 		
-		try {
-			if (window.myAccount && window.myAccount.Id) {
-				for (var userId, i=0, items=window.preferences.data(), myId=window.myAccount.get("Id"); i<items.length; i++) {
-					// CAUTION: If restored from localStorage, convert to javascript Date objects.
-					if (typeof(items[i].ModifiedAt) === "string")
-						items[i].ModifiedAt = new Date(items[i].ModifiedAt);
-					if (typeof(items[i].CreatedAt) === "string")
-						items[i].CreatedAt = new Date(items[i].CreatedAt);
-					// Find my record
-					userId = (typeof(items[i].User) === "object") ? items[i].User.Id : items[i].User;
-					if (userId === myId)
-						window.myPreferences = window.preferences.options.set("selected", items[i]);
-				}
-				if (window.myPreferences) {
-					if (window.myPreferences.Date !== config.get("today")) {
-						// If defaulting to yesterday's preferences, update the Date field, and clear 'Id' to fire the create method.
-						window.myPreferences.Date = config.get("today");
-						window.myPreferences.Id = "";
-						window.myPreferences.id = "";
-						// WARNING: Don't forget to wipe out all of yesterday's data for other people!
-						window.preferences.reset([window.myPreferences]);
-					}
-					
-				} else {
-					// NOTE: If we haven't logged in yet today, we'll create one, then save it.
-					var startTime = (new Date()).setTimeString($('#dailyprefs input[name=StartTime]').attr('min')), 
-						endTime = (new Date()).setTimeString($('#dailyprefs input[name=EndTime]').attr('max'));
-					window.myPreferences = window.preferences.options.set("selected", window.preferences.add({
-						"User": window.myAccount.get("Id"),
-						"Date": config.get("today"),
-						"StartTime": startTime,
-						"StartTimeCode": startTime.getUTCTimeCode(),
-						"EndTime": endTime,
-						"EndTimeCode": endTime.getUTCTimeCode(),
-						"CreatedAt": new Date(),
-						"ModifiedAt": new Date()
-					}));
-				}
-				// Populate results
-				window.preferences.trigger("change");
-				// Check server
-				window.preferences.cycle();
-			}
-		} catch(e) {
-			e.event = "MyPreferences Initialization";
-			(pi||console).log(e);
-		}
-		
+							
 		// Login/Logout
 		window.account.bind("change", function(e) {
 			try {
 				if (e.action === "itemchange") {
 					var value = e.items[0].get(e.field);
-					if (e.field === "Id" && value)
-						window.preferences.cycle();
+					if (e.field === "Id" && value) {
+						try {
+							for (var userId, i=0, items=window.preferences.data(), myId=value; i<items.length; i++) {
+								// CAUTION: If restored from localStorage, convert to javascript Date objects.
+								if (typeof(items[i].ModifiedAt) === "string")
+									items[i].ModifiedAt = new Date(items[i].ModifiedAt);
+								if (typeof(items[i].CreatedAt) === "string")
+									items[i].CreatedAt = new Date(items[i].CreatedAt);
+								// Find my record
+								userId = (typeof(items[i].User) === "object") ? items[i].User.Id : items[i].User;
+								if (userId === myId)
+									window.myPreferences = window.preferences.options.set("selected", items[i]);
+							}
+							if (window.myPreferences) {
+								if (window.myPreferences.Date !== config.get("today")) {
+									// If defaulting to yesterday's preferences, update the Date field, and clear 'Id' to fire the create method.
+									window.myPreferences.Date = config.get("today");
+									window.myPreferences.OptOut = true; // Default to "out" so people have to input an answer.
+									window.myPreferences.CreatedAt = null; // WARNING: Don't set these during creation!!
+									window.myPreferences.ModifiedAt = null; // WARNING: Don't set these during creation!!
+									window.myPreferences.Id = "";
+									window.myPreferences.id = "";
+									// WARNING: Don't forget to wipe out all of yesterday's data for other people!
+									window.preferences.reset([window.myPreferences]);
+								}
+								
+							} else {
+								// NOTE: If we haven't logged in yet today, we'll create one, then save it.
+								var startTime = (new Date()).setTimeString($('#dailyprefs input[name=StartTime]').attr('min')), 
+									endTime = (new Date()).setTimeString($('#dailyprefs input[name=EndTime]').attr('max'));
+								window.myPreferences = window.preferences.options.set("selected", window.preferences.add({
+									"User": value,
+									"Date": config.get("today"),
+									"StartTime": startTime,
+									"StartTimeCode": startTime.getUTCTimeCode(),
+									"EndTime": endTime,
+									"EndTimeCode": endTime.getUTCTimeCode(),
+									// "CreatedAt": new Date(), // WARNING: Don't set these during creation!!
+									// "ModifiedAt": new Date(), // WARNING: Don't set these during creation!!
+									"OptOut": true // Default to "out" so people have to input an answer.
+								}));
+							}
+							// Populate results
+							window.preferences.trigger("change");
+							// Check server
+							window.preferences.cycle();
+						} catch(e) {
+							e.event = "MyPreferences Initialization";
+							(pi||console).log(e);
+						}
+					}
 				} else if (e.action === "remove") {
 					if (window.preferences.timeout)
 						window.clearTimeout(window.preferences.timeout);
+					delete window.myPreferences;
 					window.preferences.reset();
 				}
 			} catch(e) {
 				e.event = "Preference Account Change";
 				(pi||console).log(e);
 			}
-		});
+		}).trigger("change", { action: "itemchange", field: "Id", items: [window.myAccount] });
 	} catch(e) {
 		e.event = "Daily Preferences Instantiation";
 		(pi||console).log(e);
